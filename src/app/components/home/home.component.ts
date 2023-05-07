ef12468648e408b790e67fc08470098159b1ddd0
Atmiyata <jayrajsolanki50@gmail.com>
2023-05-06 21:45:26 -0400

    Category component template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BannerComponent } from './banner/banner.component';
+import { CategoryComponent } from '../category/category.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, BannerComponent],
+  imports: [CommonModule, BannerComponent, CategoryComponent],
 })
 export class HomeComponent {}
